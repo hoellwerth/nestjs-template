f6f8b92bea8f34025d246fb8392e1dff02edb3df
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2022-12-21 21:53:56 +0100

    Added AuthControllerTests

--- src/auth/controller/auth.controller.spec.ts
+++ src/auth/controller/auth.controller.spec.ts
@@ -1,5 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from '../services/auth.service';
+import { AuthServiceMock } from '../services/auth.service.spec';
+import { UserService } from '../../user/services/user.service';
+import {
+  mockUser,
+  UserServiceMock,
+} from '../../user/services/user.service.spec';
+import { VerifyStrategy } from '../strategy/verify.strategy';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +15,20 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: AuthServiceMock,
+        },
+        {
+          provide: UserService,
+          useValue: UserServiceMock,
+        },
+        {
+          provide: VerifyStrategy,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -14,5 +36,28 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should login', async () => {
+    const authServiceSpy = jest
+      .spyOn(AuthServiceMock, 'login')
+      .mockResolvedValue('token');
+
+    const user = jest
+      .spyOn(UserServiceMock, 'getUserByName')
+      .mockResolvedValue({ ...mockUser(), _id: 'id' });
+
+    const result = await controller.login({
+      user: mockUser(),
+    });
+
+    expect(result).toEqual({
+      access_token: 'token',
+      id: 'id',
+      role: 'user',
+    });
+
+    expect(authServiceSpy).toBeCalledWith(mockUser());
+    expect(user).toBeCalledWith(mockUser().username);
   });
 });
