ee10062a4864498546d0bfbf73b24e62409be244
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2022-12-26 22:53:08 +0100

    Added log method

--- src/log/services/log.service.ts
+++ src/log/services/log.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class LogService {}
+export class LogService {
+  log(title: string, message: string, before: number) {
+    console.log(
+      `\x1b[32m[Nest] ${
+        process.pid
+      }  -\x1b[0m ${new Date().toLocaleDateString()}, ${new Date().toLocaleTimeString()}`,
+      `\x1b[32m    LOG\x1b[0m`,
+      `\x1b[33m[${title}]\x1b[0m`,
+      `\x1b[32m${message}\x1b[0m`,
+      `\x1b[33m+${Date.now() - before}ms\x1b[0m`,
+    );
+  }
+}
