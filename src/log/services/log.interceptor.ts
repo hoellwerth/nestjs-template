f1e9b396b32c5e73a9b7790a9c16380a004bbbf8
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2022-12-26 22:24:59 +0100

    Added LogInterceptor

---
+++ src/log/services/log.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class LogInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const now = Date.now();
+
+    return next.handle().pipe(
+      tap(() => {
+        console.log(
+          `\x1b[32m[Nest] ${
+            process.pid
+          }  -\x1b[0m ${new Date().toLocaleDateString()}, ${new Date().toLocaleTimeString()}`,
+          `\x1b[32m    LOG\x1b[0m`,
+          `\x1b[33m[RouteCalled]\x1b[0m`,
+          `\x1b[32mCalled {${context.getArgs()[0].url}`,
+          `${context.getArgs()[0].method}},`,
+          `Client: {${context.getArgs()[0].headers['user-agent']},`,
+          `${context.getArgs()[0].ip}}`,
+          `\x1b[33m${Date.now() - now}ms\x1b[0m`,
+        );
+      }),
+    );
+  }
+}
