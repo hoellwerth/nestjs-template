829fbe7311190f140398c14804db42509a878e78
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-01-05 23:08:23 +0100

    Fixed UserStub

--- test/stubs/user.stub.ts
+++ test/stubs/user.stub.ts
@@ -4,13 +4,13 @@ import * as crypto from 'crypto';
 export const userStub = (
   username = 'Test',
   email = 'test@test.eu',
-  password = '71e7a68b1fd3d6e9ac3de4fd7ff0c450da5c72682e4c6b8672aa7e480841873f684f08d7e3db0159134d6a6d9cbe19b9d6a80c57e36c5cc9bf160e89b0a8e025',
+  password = hash('Test12345678' + 'salt'),
   role = 'user',
   token: string | null = null,
 ): User => ({
   username,
   email,
-  password: hash(password),
+  password,
   role,
   token,
 });
